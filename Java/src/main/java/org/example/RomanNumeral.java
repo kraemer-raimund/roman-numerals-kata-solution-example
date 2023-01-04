bb6aeade7c9a74904850cae8e1a0ee8880b6affd
Raimund Krämer <kraemer_raimund@web.de>
2023-01-04 14:30:28 +0100

    Add RomanNumeral stub and a failing example unit test

---
+++ Java/src/main/java/org/example/RomanNumeral.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class RomanNumeral {
+
+    public RomanNumeral(int value) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    public RomanNumeral(String roman) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    public RomanNumeral plus(RomanNumeral toAdd) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+}
