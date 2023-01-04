f860d29551e5a9ab288107efb19f9a43c9dafd20
Raimund Krämer <kraemer_raimund@web.de>
2023-01-04 14:52:58 +0100

    Disable example unit test
    It can act as a reference for testing syntax for now, but it assumes
    too much functionality that will only be added after multiple TDD
    iterations.

--- Java/src/test/java/org/example/RomanNumeralTest.java
+++ Java/src/test/java/org/example/RomanNumeralTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,6 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RomanNumeralTest {
 
     @Test
+    @Disabled("This test acts as an example, but does not make sense " +
+            "until a much later TDD iteration.")
     void addingTwoNumeralsYieldsTheirSum() {
         var roman2 = new RomanNumeral(2);
         var roman3 = new RomanNumeral("III");
