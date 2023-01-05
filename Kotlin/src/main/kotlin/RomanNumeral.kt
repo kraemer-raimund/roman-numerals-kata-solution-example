af00a319d383e1659b338ea134d560968420b997
Raimund Krämer <kraemer_raimund@web.de>
2023-01-05 17:25:46 +0100

    Implement conversion to int for value 1

--- Kotlin/src/main/kotlin/RomanNumeral.kt
+++ Kotlin/src/main/kotlin/RomanNumeral.kt
@@ -2,6 +2,12 @@ package org.example
 
 class RomanNumeral(private val value: Int) {
 
+    constructor(roman: String) : this(1)
+
+    fun toInt(): Int {
+        return 1;
+    }
+
     override fun toString(): String {
         return "I";
     }
--- Kotlin/src/test/kotlin/RomanNumeralTest.kt
+++ Kotlin/src/test/kotlin/RomanNumeralTest.kt
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test
 class RomanNumeralTest {
 
     @Test
+    fun romanStringCanBeConvertedToInt() {
+        val roman1 = RomanNumeral("I");
+        val intValue = roman1.toInt();
+        assertThat(intValue).isEqualTo(1);
+    }
+
+    @Test
     fun intCanBeConvertedToRomanString() {
         val roman1 = RomanNumeral(1)
         val romanString = roman1.toString()
