fdc20156d21e9e032c6239389f5a90c011cb7002
Raimund Krämer <kraemer_raimund@web.de>
2023-01-05 16:58:46 +0100

    Delete example unit test

--- Kotlin/src/test/kotlin/RomanNumeralTest.kt
+++ Kotlin/src/test/kotlin/RomanNumeralTest.kt
@@ -4,14 +4,5 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class RomanNumeralTest {
-
-    @Test
-    fun addingTwoNumeralsYieldsTheirSum() {
-        val roman2 = RomanNumeral(2)
-        val roman3 = RomanNumeral("III")
-
-        val romanSum = roman2 + roman3
-
-        assertThat(romanSum.toString()).isEqualTo("V")
-    }
+    
 }
