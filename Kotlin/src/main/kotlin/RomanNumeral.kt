2c9e12740eea1c1d64e5c6a6aa688c9d94c26cbf
Raimund Krämer <kraemer_raimund@web.de>
2023-01-05 17:00:51 +0100

    Delete stub code

--- Kotlin/src/main/kotlin/RomanNumeral.kt
+++ Kotlin/src/main/kotlin/RomanNumeral.kt
@@ -1,20 +1,5 @@
 package org.example
 
 class RomanNumeral {
-
-    constructor(value: Int) {
-        throw UnsupportedOperationException("Not yet implemented.")
-    }
-
-    constructor(roman: String) {
-        throw UnsupportedOperationException("Not yet implemented.")
-    }
-
-    operator fun plus(toAdd: RomanNumeral): RomanNumeral {
-        throw UnsupportedOperationException("Not yet implemented.")
-    }
-
-    override fun toString(): String {
-        throw UnsupportedOperationException("Not yet implemented.")
-    }
+    
 }
