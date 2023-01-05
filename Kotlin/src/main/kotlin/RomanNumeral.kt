25f1ad548a05c419df898aeee7c7eb9bc70e3f0d
Raimund Krämer <kraemer_raimund@web.de>
2023-01-05 18:05:03 +0100

    Implement equals and hashCode for RomanNumeral
    For hashCode, simply returning the int value is sufficient, since
    hashCodes are ints that should just be as spread as possible. Other
    algorithms would of course work as well, e. g. scaling by a prime
    number, but are not necessary in case of a single int property.

--- Kotlin/src/main/kotlin/RomanNumeral.kt
+++ Kotlin/src/main/kotlin/RomanNumeral.kt
@@ -33,6 +33,16 @@ class RomanNumeral {
         }
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as RomanNumeral
+        if (value != other.value) return false
+        return true
+    }
+
+    override fun hashCode(): Int = value
+
     private fun parse(roman: String): Int {
         return when (roman) {
             "I" -> 1
--- Kotlin/src/test/kotlin/RomanNumeralTest.kt
+++ Kotlin/src/test/kotlin/RomanNumeralTest.kt
@@ -1,5 +1,6 @@
 package org.example
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -26,5 +27,22 @@ class RomanNumeralTest {
         val sum = roman2 + roman3
         assertThat(sum.toString()).isEqualTo("V")
         assertThat(sum.toInt()).isEqualTo(5)
+    }
+
+    @Test
+    fun equalsAndHashCode() {
+        val roman1a = RomanNumeral(1)
+        val roman1b = RomanNumeral("I")
+        val roman2 = RomanNumeral(2)
+
+        // Note:
+        // Two equal objects must have the same hash code, but two unequal objects
+        // are *NOT* required to have different hash codes!
+        assertThat(roman1a.hashCode())
+            .isEqualTo(roman1b.hashCode())
+        Assertions.assertThatObject(roman1a)
+            .isEqualTo(roman1a)
+            .isEqualTo(roman1b)
+            .isNotEqualTo(roman2)
     }
 }
